b013b0b3e7b7fe025ab3a794b45f8a386921e10c
Artem <artemklinkov@yandex.ru>
2021-03-04 14:22:11 +0300

    Animation

--- src/js/app.js
+++ src/js/app.js
@@ -10,23 +10,3 @@ chat.init();
 
 const liker = new Liker();
 liker.init();
-
-const elButtons = document.querySelector('.buttons');
-const elements = document.querySelectorAll('.elements');
-const elCollapse = document.querySelector('.collapse');
-const elCallback = document.querySelector('.callback');
-const elLker = document.querySelector('.liker');
-
-elButtons.addEventListener('click', (e) => {
-  for (const item of elements) {
-    item.classList.add('none');
-  }
-  const elClassList = e.target.id;
-  if (elClassList === 'callback') {
-    elCallback.classList.remove('none');
-  } else if (elClassList === 'liker') {
-    elLker.classList.remove('none');
-  } else {
-    elCollapse.classList.remove('none');
-  }
-});
